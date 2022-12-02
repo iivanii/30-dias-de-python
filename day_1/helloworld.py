5b9c29e8242af36b8bf453c3b7c1699519f7c884
iivanii <ivanbsibs7073@gmail.com>
2022-12-02 11:13:13 +0100

    guardar

---
+++ day_1/helloworld.py
@@ -0,0 +1,38 @@
+print (3+4)
+print(3-4)
+print (3*4)
+print (3%4)
+print (3/4)
+print (3**4)
+print (3//4)
+
+print ('Iván')
+print ('Betanzos')
+print ('Spain')
+print ('I am enjoying 30 days of python')
+
+print (type (10))
+print (type (9.8))
+print (type (3.14))
+print (type(4-4j))
+print (type(['Asabeneh', 'Python', 'Finland']))
+print (type('Iván'))
+print (type('Betanzos'))
+print (type('Spain'))
+
+print (12+7)
+print (16/9)
+print (23-10J)
+print ('computer')
+True # Is the dog sleep? If it is sleep, then value is true
+False # Is the dog sleep? If it is not sleep, then value is false
+print(type (['board', 'sun', 'Mexico']))
+print ('table', 'computer', 'mouse', 'pen') #things
+print ({1, 3, 16, 24, 6})
+
+{'first name': 'Iván',
+'last name': 'Betanzos',
+'country': 'Spain',
+'age': '15',
+'is married': False,
+'skills': ['chess', 'technology', 'ping pong']}
