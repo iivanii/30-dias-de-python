07c346f0eb42af22c55a3278bc4b3a991f32a29c
iivanii <ivanbsibs7073@gmail.com>
2022-12-23 10:21:14 +0100

    guardar.

---
+++ day_2/Variables.py
@@ -0,0 +1,52 @@
+print ('Day 2: 30 Days of python programming')
+first_name = 'Iván'
+last_name = 'Betanzos'
+full_name = 'Iván Betanzos Soto'
+country = 'Sapin'
+city = 'Jerez de la Frontera'
+age = 16
+cear = 2022
+is_married = False
+is_light_on = True
+hobbie, pet, favourite_sport = 'videogames', 'hamster', 'basketball'
+
+print (type(first_name))
+print (type(last_name))
+print (type(full_name))
+print (type(Country))
+print (type(City))
+print (type(Age))
+print (type(Year))
+print (type(Is_married))
+print (type(Is_light_on))
+print (type(hobbie))
+print (type(pet))
+print (type(favourite_sport))
+
+print(len(first_name))
+print(len(first_name), len(last_name))
+
+num_one=5
+num_two=4
+total=(num_one+num_two)
+diff=(num_one-num_two)
+product=(num_one*num_two)
+division=(num_one/num_two)
+remainder=(num_two%num_one)
+exp=(num_one**num_two)
+floor_dision=(num_one//num_two)
+
+area_of_circle=(3.1416*30**2)
+circum_of_circle=(3.1416*60)
+input(3.1416*30**2)
+
+print(input(first_name, last_name, country, age))
+
+
+
+
+
+
+
+
+
