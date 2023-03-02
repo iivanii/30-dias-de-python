24f4caca1d3139cb6c55ac9d08a0efb398f03469
iivanii <ivanbsibs7073@gmail.com>
2023-03-02 09:10:31 +0100

    day_5

---
+++ day_5/list.py
@@ -0,0 +1,15 @@
+#1. Declare an empty list
+list=[]
+print(list)
+
+#2. Declare a list with more than 5 items
+list_5i=['computer', 'apple', 'mouse','table','chair', 'school']
+print(list_5i)
+
+#3. Find the length of your list
+length=len(list_5i)
+print(length)
+
+#4. Get the first item, the middle item and the last item of the list
+
+print(length[0])
