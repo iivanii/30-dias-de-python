22c0cf614056a13f81ff915d165ab554cbe8e8a9
iivanii <ivanbsibs7073@gmail.com>
2023-03-15 18:40:23 +0100

    day_6

---
+++ day_6/tuples.py
@@ -0,0 +1,16 @@
+#1. Create an empty tuple
+empy_tuple=()
+
+#2. Create a tuple containing names of your sisters and your brothers (imaginary siblings are fine)
+family=('Cristian','Pablo','Isabel','Laura')
+
+#3. Join brothers and sisters tuples and assign it to siblings
+brothers=('Cristian','Pablo')
+sisters=('Isabel','Laura')
+siblings=brothers+sisters
+
+#4. How many siblings do you have?
+len(siblings)
+
+#5. Modify the siblings tuple and add the name of your father and mother and assign it to family_members
+family_members=siblings+('Federico','Sara')
